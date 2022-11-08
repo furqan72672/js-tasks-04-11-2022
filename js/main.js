43f0463463f6499ec63c4aa61e0f38b253cdef22
Muhammad Furqan Munir <furqan72672@gmail.com>
2022-11-08 14:27:31 +0500

    this commit will add html input and js function to sum of all positve numbers in an array as in task 2

--- js/main.js
+++ js/main.js
@@ -9,3 +9,14 @@ function findNumberType(){
 }
 
 
+
+let arr=[1,-1,0,-3,5]
+document.getElementById("sumOfPositiveInput").innerHTML="Array is => ["+arr+"]"
+function sumOfPositive(){
+    let s=0
+    arr.forEach(elem => {
+        if(elem<=0)return
+        s+=elem
+    });
+    document.getElementById("sumOfPositiveRes").innerHTML="Sum of positives in array = "+s
+}
